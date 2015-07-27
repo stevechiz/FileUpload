f3d9f50c4a7093514a1dca3dbad2a38c3051f7fa
stevechiz <steve@chissell.name>
2015-07-27 17:51:59 +0100

    Add missing file

---
+++ File Upload/Services/IFileImporter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace File_Upload.Services
+{
+    public interface IFileImporter
+    {
+
+        int NumberImported { get; set; }
+
+        int NumberProcessed { get; set; }
+
+        int NumberSkipped { get; set; }
+
+        int NumberFailed { get; set; }
+
+        List<string> FailedRecords { get; set; }
+
+        bool ProcessFile(FileStream inputFile);
+    }
+}
